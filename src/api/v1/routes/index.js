7cdb4425dc491cc978356c8afade1070fddc3c24
Sahil Prajapati <sprajapati14012002@gmail.com>
2021-09-01 22:58:15 +0530

    final commit project finished

--- src/api/v1/routes/index.js
+++
@@ -1,9 +0,0 @@
-const { Router } = require('express');
-const router = Router();
-const logger = require('../../../logger');
-const passport = require('passport');
-
-router.use('/user', require('./user'));
-router.use('/flamspark1', passport.authenticate('jwt', { session:false }), require('./serviceA'));
-router.use('/flamspark2', passport.authenticate('jwt', { session:false }), require('./serviceB'));
-module.exports = router;
