49684932efab394051087d078668e3e52173ec1a
Sahil Prajapati <sprajapati14012002@gmail.com>
2021-09-01 11:47:16 +0530

    repo initialized

---
+++ src/index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const app = express();
+
+const logger = require('./logger');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get('/',(req,res) => {
+    logger.info('Welcome');
+    logger.error('error occured');
+    logger.debug('debugging')
+    res.send("hello")
+})
+
+app.use('/api', require('./api'));
+
+app.listen(process.env.PORT, (err) => {
+    if (err) {
+        logger.error(err);
+    }
+    logger.info('Server started');
+});
