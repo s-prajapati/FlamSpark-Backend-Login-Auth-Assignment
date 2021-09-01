7892bf30402e1847e998b324e6a42cfb46fe9581
Sahil Prajapati <sprajapati14012002@gmail.com>
2021-09-01 13:08:32 +0530

    user model prepared

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
-
 const logger = require('./logger');
+const db = require('./config/mongoose')
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
